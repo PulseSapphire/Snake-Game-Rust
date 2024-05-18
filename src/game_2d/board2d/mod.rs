6d2325ce7aafdc53d6f4d04836ce00b87b095100
PulseSapphire <souravdube17122003@gmail.com>
2024-05-19 04:19:40 +0530

    add method to create a new instance of Layout2D to the layout module.
    add a method to create a new board struct instance alongside modifying signature of struct with const generics for the layout2d field.

--- src/game_2d/board2d/mod.rs
+++ src/game_2d/board2d/mod.rs
@@ -1,14 +1,24 @@
-use layout::Layout;
-use crate::cli_game::snake::Snake;
 use crate::game::types::Position2D;
+use crate::game_2d::board2d::layout::Layout2D;
+use crate::game_2d::snake::Snake2D;
 
 mod layout;
 
-const BOARD_WIDTH: u8 = u8::MAX;
-const BOARD_HEIGHT: u8 = u8::MAX;
+const DEFAULT_BOARD_WIDTH: u8 = u8::MAX;
+const DEFAULT_BOARD_HEIGHT: u8 = u8::MAX;
 
-pub struct Board2D {
-    snake: Snake,
-    layout: Layout,
+pub struct Board2D<const W: u8, const H: u8> {
+    snake: Snake2D,
+    layout: Layout2D<W, H>,
     food: Position2D,
+}
+
+impl <const W: u8, const H: u8> Board2D<W, H> {
+    pub fn new (snake: Snake2D, food: Position2D) -> Self {
+        Self {
+            snake,
+            food,
+            layout: Layout2D::<W, H>::new(),
+        }
+    }
 }
