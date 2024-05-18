7c92ad6580fa58ca6a9ac92e558d1b5f5ade4942
PulseSapphire <souravdube17122003@gmail.com>
2024-05-19 02:57:43 +0530

    rename Position and Direction to Position2D and Direction2D.

--- src/game/types.rs
+++ src/game/types.rs
@@ -1,4 +1,4 @@
-pub enum Direction {
+pub enum Direction2D {
     Up,
     Down,
     Left,
@@ -6,7 +6,7 @@ pub enum Direction {
     Stationary,
 }
 
-pub struct Position {
+pub struct Position2D {
     pub x: u8,
     pub y: u8,
 }
