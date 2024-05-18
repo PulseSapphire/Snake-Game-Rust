2ed25650709f254f4094747d953f33087328105e
PulseSapphire <souravdube17122003@gmail.com>
2024-05-19 02:47:55 +0530

    extract Direction and Position to types.rs.

--- src/cli_game/snake.rs
+++ src/cli_game/snake.rs
@@ -1,15 +1,4 @@
-pub enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-    Stationary,
-}
-
-pub struct Position {
-    pub x: u8,
-    pub y: u8,
-}
+use crate::cli_game::types::{Direction, Position};
 
 pub struct Snake {
     head_position: Position,
---
+++ src/cli_game/types.rs
@@ -0,0 +1,12 @@
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    Stationary,
+}
+
+pub struct Position {
+    pub x: u8,
+    pub y: u8,
+}
