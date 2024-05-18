aaebdef92f488ba1f1cfa1ace911c7179f98703b
PulseSapphire <souravdube17122003@gmail.com>
2024-05-19 04:12:35 +0530

    add method to create a new instance of Layout2D to the layout module.

--- src/game_2d/board2d/layout.rs
+++ src/game_2d/board2d/layout.rs
@@ -1,3 +1,14 @@
+use std::ptr::write_unaligned;
+
 pub struct Layout2D<const W: u8, const H: u8> {
     layout: [[u16; H as usize]; W as usize],
+}
+
+impl <const W: u8, const H: u8> Layout2D<W, H> {
+    pub fn new() -> Self {
+        Self {
+            layout: [[0; H as usize]; W as usize]
+        }
+    }
+
 }
