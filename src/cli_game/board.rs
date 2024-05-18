01f352c146165800ef6b761b0a4f7393eabc27e6
PulseSapphire <souravdube17122003@gmail.com>
2024-05-19 02:49:21 +0530

    create Board module.
    create Board module alongside Board struct for keeping track of snake, food and the layout.

--- src/cli_game/board.rs
+++ src/cli_game/board.rs
@@ -1,9 +1,11 @@
 use crate::cli_game::snake::Snake;
+use crate::cli_game::types::Position;
 
 const BOARD_WIDTH: u8 = u8::MAX;
 const BOARD_HEIGHT: u8 = u8::MAX;
 
 pub struct Board {
     snake: Snake,
-    layout: [[u16; BOARD_HEIGHT as usize]; BOARD_WIDTH as usize]
+    layout: [[u16; BOARD_HEIGHT as usize]; BOARD_WIDTH as usize],
+    food: Position,
 }
