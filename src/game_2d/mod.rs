18486ba506e4605f5f4068a7022c3e804ef7451e
PulseSapphire <souravdube17122003@gmail.com>
2024-05-20 01:54:15 +0530

    start creation of Game2D struct.

--- src/game_2d/mod.rs
+++ src/game_2d/mod.rs
@@ -1,3 +1,13 @@
+use crate::game_2d::board2d::Board2D;
+use crate::game_2d::snake::Snake2D;
+use crate::game_2d::snake_controller::SnakeController2D;
+
 mod board2d;
 mod snake;
 mod snake_controller;
+
+pub struct Game2D<'a, const W: usize, const H: usize> {
+    board: Board2D<W, H>,
+    snake: Snake2D,
+    snake_controller: SnakeController2D<'a, W, H>,
+}
