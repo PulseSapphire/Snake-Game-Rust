f61dd5c0085930c7214fb4ae317d0322e5dc3a45
PulseSapphire <souravdube17122003@gmail.com>
2024-05-20 01:53:01 +0530

    add several new getters and setters for Snake2D.

--- src/game_2d/snake/mod.rs
+++ src/game_2d/snake/mod.rs
@@ -15,8 +15,16 @@ impl Snake2D {
     pub fn get_direction(&self) -> &Direction2D {
         &self.direction
     }
+
+    pub fn set_direction(&mut self, direction: Direction2D) {
+        self.direction = direction;
+    }
     pub fn get_tail_position(&self) -> &Position2D {
         &self.tail_position
+    }
+
+    pub fn set_tail_position(&mut self, tail_position: Position2D) {
+        self.tail_position = tail_position;
     }
     pub fn get_head_position(&self) -> &Position2D {
         &self.head_position
@@ -27,5 +35,15 @@ impl Snake2D {
     }
     pub fn get_head_position_mut(&mut self) -> &mut Position2D {
         &mut self.head_position
+    }
+
+    pub fn get_move_tail(&self) -> bool {
+        self.move_tail
+    }
+    pub fn set_move_tail(&mut self, move_tail: bool) {
+        self.move_tail = move_tail;
+    }
+    pub fn increment_length(&mut self) {
+        self.length += 1;
     }
 }
