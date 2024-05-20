89efb4a38dcd77d289a5ca98c1816c03d4df42f6
PulseSapphire <souravdube17122003@gmail.com>
2024-05-21 01:05:45 +0530

    create layout_manager module.

--- src/game_2d/mod.rs
+++ src/game_2d/mod.rs
@@ -5,7 +5,8 @@ use crate::game_2d::snake_controller::SnakeController2D;
 
 mod board2d;
 mod snake;
-mod snake_controller;
+pub mod snake_controller;
+mod layout_manager;
 
 pub struct Game2D<const W: usize, const H: usize> {
     snake_controller: SnakeController2D<W, H>,
