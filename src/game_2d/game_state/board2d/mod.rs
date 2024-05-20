c4930af593f70728f0e54916fa1b8d1dbcd91037
PulseSapphire <souravdube17122003@gmail.com>
2024-05-21 04:13:42 +0530

    remove old Board2D and rename Layout2D to new Board2D.
    - Remove the redundant Board2D struct that previously contained Layout2D and food position.
    - Rename Layout2D to Board2D to more accurately represent its purpose as the game board.
    - Move food position management to GameState struct to adhere to the Single Responsibility Principle.
    - Update all relevant references and methods to use the new Board2D struct.

--- src/game_2d/game_state/board2d/mod.rs
+++ src/game_2d/game_state/board2d/mod.rs
@@ -1,42 +1,124 @@
 use crate::game::types::Position2D;
-use crate::game_2d::game_state::board2d::layout::Layout2D;
-
-mod layout;
 
 pub struct Board2D<const W: usize, const H: usize> {
-    layout: Layout2D<W, H>,
-    food: Position2D,
+    layout: [[u16; H]; W],
 }
 
 impl<const W: usize, const H: usize> Board2D<W, H> {
-    const U8_MAX_IN_USIZE: usize = u8::MAX as usize;
-    pub fn new(food: Position2D) -> Self {
-        if W > Self::U8_MAX_IN_USIZE || H > Self::U8_MAX_IN_USIZE {
-            panic!(
-                "Cannot use dimensions bigger than {}",
-                Self::U8_MAX_IN_USIZE
-            );
-        }
-
+    pub fn new() -> Self {
         Self {
-            food,
-            layout: Layout2D::<W, H>::new(),
+            layout: [[0; H]; W],
         }
     }
 
-    pub fn get_width(&self) -> u8 {
-        self.layout.get_width()
+    pub fn get_val_at_pos(&self, position: &Position2D) -> u16 {
+        self.layout[position.x as usize][position.y as usize]
     }
 
-    pub fn get_height(&self) -> u8 {
-        self.layout.get_height()
+    pub fn get_val_at_index(&self, x: usize, y: usize) -> u16 {
+        self.layout[x][y]
     }
 
-    pub fn get_layout(&self) -> &Layout2D<W, H> {
+    pub fn set_val_at_index(&mut self, x: usize, y: usize, new_value: u16) {
+        self.layout[x][y] = new_value;
+    }
+
+    pub fn get_layout(&self) -> &[[u16; H]] {
         &self.layout
     }
 
-    pub fn get_layout_mut(&mut self) -> &mut Layout2D<W, H> {
-        &mut self.layout
+    const WIDTH_U8: u8 = W as u8;
+    pub fn get_width(&self) -> u8 {
+        Self::WIDTH_U8
+    }
+
+    const HEIGHT_U8: u8 = H as u8;
+    pub fn get_height(&self) -> u8 {
+        Self::HEIGHT_U8
+    }
+
+    pub fn get_max_adjacent_value(&self, position: &Position2D) -> u16 {
+        let x = position.x as usize;
+        let y = position.y as usize;
+
+        self.layout[x + 1][y]
+            .max(self.layout[x - 1][y].max(self.layout[x][y + 1].max(self.layout[x][y - 1])))
+    }
+
+    pub fn get_min_adjacent_value(&self, position: &Position2D) -> u16 {
+        let x = position.x as usize;
+        let y = position.y as usize;
+
+        self.layout[x + 1][y]
+            .min(self.layout[x - 1][y].min(self.layout[x][y + 1].min(self.layout[x][y - 1])))
+    }
+
+    pub fn get_max_adjacent_position(&self, position: &Position2D) -> Position2D {
+        let x = position.x as usize;
+        let y = position.y as usize;
+
+        let mut max_pos = Position2D { x: 0, y: 0 };
+        let mut max_val = u16::MIN;
+
+        let adjacent_positions = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)];
+
+        for (adj_x, adj_y) in adjacent_positions {
+            let val = self.layout[adj_x][adj_y];
+            if val > max_val {
+                max_val = val;
+                max_pos = Position2D {
+                    x: adj_x as u8,
+                    y: adj_y as u8,
+                };
+            }
+        }
+
+        max_pos
+    }
+
+    pub fn get_min_adjacent_position(&self, position: &Position2D) -> Position2D {
+        let x = position.x as usize;
+        let y = position.y as usize;
+
+        let mut min_pos = Position2D { x: 0, y: 0 };
+        let mut min_val = u16::MIN;
+
+        let adjacent_positions = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)];
+
+        for (adj_x, adj_y) in adjacent_positions {
+            let val = self.layout[adj_x][adj_y];
+            if val < min_val {
+                min_val = val;
+                min_pos = Position2D {
+                    x: adj_x as u8,
+                    y: adj_y as u8,
+                };
+            }
+        }
+
+        min_pos
+    }
+
+    pub fn get_adjacent_position_with_val(
+        &self,
+        position: &Position2D,
+        target_value: u16,
+    ) -> Option<Position2D> {
+        let x = position.x as usize;
+        let y = position.y as usize;
+
+        let adjacent_positions = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)];
+
+        for (adj_x, adj_y) in adjacent_positions {
+            let val = self.layout[adj_x][adj_y];
+            if val == target_value {
+                return Some(Position2D {
+                    x: adj_x as u8,
+                    y: adj_y as u8,
+                });
+            }
+        }
+
+        None
     }
 }
