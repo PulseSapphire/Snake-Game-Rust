73a7e13c83b80c2880a7e8b00d04a6c24b9f54f7
PulseSapphire <souravdube17122003@gmail.com>
2024-05-21 02:36:36 +0530

    add Result return type to OnSnakeMove::on_event.

--- src/game_2d/snake_controller/snake_controller_observers.rs
+++ src/game_2d/snake_controller/snake_controller_observers.rs
@@ -1,5 +1,5 @@
 use crate::game::types::{Direction2D, Position2D};
 
 pub trait OnSnakeMove {
-    fn on_event (&mut self, last_position: &Position2D, new_position: &Position2D, direction: &Direction2D, grow: bool);
+    fn on_event (&mut self, last_position: &Position2D, new_position: &Position2D, direction: &Direction2D, grow: bool) -> Result<(), &'static str>;
 }
