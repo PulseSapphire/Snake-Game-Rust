ba1fb61b04b0f57b6c429ec9a9f854f02c996e5e
PulseSapphire <souravdube17122003@gmail.com>
2024-05-24 20:45:15 +0530

    add getters for GameState.

--- src/game_2d/game_state/mod.rs
+++ src/game_2d/game_state/mod.rs
@@ -1,9 +1,43 @@
-use crate::game_2d::board2d::Board2D;
-use crate::game_2d::food::Food;
-use crate::game_2d::snake::Snake2D;
+use food::Food;
+use snake::Snake2D;
+use crate::game_2d::game_state::board2d::Board2D;
 
-struct GameState <const W: usize, const H: usize> {
+pub mod snake;
+pub mod board2d;
+mod food;
+
+pub struct GameState <const W: usize, const H: usize> {
     snake: Snake2D,
     board: Board2D<W, H>,
     food: Food
+}
+
+impl <const W: usize, const H: usize> GameState<W, H> {
+    fn new (snake: Snake2D, board: Board2D<W, H>, food: Food) -> Self {
+        Self {
+            snake,
+            board,
+            food
+        }
+    }
+
+    pub fn get_snake(&self) -> &Snake2D {
+        &self.snake
+    }
+    pub fn get_board(&self) -> &Board2D<W, H> {
+        &self.board
+    }
+    pub fn get_food(&self) -> &Food {
+        &self.food
+    }
+
+    pub fn get_mut_snake(&mut self) -> &mut Snake2D {
+        &mut self.snake
+    }
+    pub fn get_mut_board(&mut self) -> &mut Board2D<W, H> {
+        &mut self.board
+    }
+    pub fn get_mut_food(&mut self) -> &mut Food {
+        &mut self.food
+    }
 }
