8698ec2e4f73560791c5ebb8d694c684d0efc272
PulseSapphire <souravdube17122003@gmail.com>
2024-05-28 20:43:41 +0530

    change FoodController::spawn_food to return a result.

--- src/game/engine/game_controller/food_controller.rs
+++ src/game/engine/game_controller/food_controller.rs
@@ -1,6 +1,6 @@
 use crate::game::types::Position2D;
 
 pub trait FoodController {
-    fn spawn_food(&mut self, position: &Position2D);
+    fn spawn_food(&mut self, position: &Position2D) -> Result<(), &'static str>;
     fn spawn_food_random(&mut self);
 }
