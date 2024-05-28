136435d5efb785b96b8d7a1f7ac1df6337838269
PulseSapphire <souravdube17122003@gmail.com>
2024-05-28 05:44:09 +0530

    update FoodController trait.
    - change FoodController::spawn_food to take a Position2D parameter.
    - create new method FoodController::spawn_food_random to randomly spawn food.

--- src/game/engine/game_controller/food_controller.rs
+++ src/game/engine/game_controller/food_controller.rs
@@ -1,3 +1,6 @@
+use crate::game::types::Position2D;
+
 pub trait FoodController {
-    fn spawn_food(&mut self);
+    fn spawn_food(&mut self, position: Position2D);
+    fn spawn_food_random(&mut self);
 }
