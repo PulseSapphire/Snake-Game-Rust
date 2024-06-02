caca5ad8d434ebf347eb8e4f3ef3b6a27e8f41c3
PulseSapphire <souravdube17122003@gmail.com>
2024-06-02 21:01:29 +0530

    extend EventHandler for OnBoardTileChange.

--- src/game/events/on_board_tile_change.rs
+++ src/game/events/on_board_tile_change.rs
@@ -1,6 +1,7 @@
 use crate::game::engine::game_state::board::board_tile::BoardTile;
+use crate::game::events::EventHandler;
 use crate::game::types::position::Position;
 
-pub trait OnBoardTileChange <P: Position> {
+pub trait OnBoardTileChange <P: Position>: EventHandler {
     fn on_event (new_board_tile: &BoardTile, last_board_tile: &BoardTile, position: &P);
 }
