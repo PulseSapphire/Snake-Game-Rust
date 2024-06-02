2651454c599b067542bcbac1b2c91169ab97eab2
PulseSapphire <souravdube17122003@gmail.com>
2024-06-02 21:21:30 +0530

    create trait game::engine::game_state::board::Board.

--- src/game/engine/game_state/board/mod.rs
+++ src/game/engine/game_state/board/mod.rs
@@ -1,1 +1,16 @@
+use crate::game::engine::game_state::board::board_tile::BoardTile;
+use crate::game::types::position::Position;
+
 pub mod board_tile;
+
+pub trait Board <P: Position> {
+    fn get_tile_at_pos(&self, position: &P) -> &BoardTile;
+
+    fn set_tile_at_pos(&mut self, position: &P, new_board_tile: BoardTile);
+
+    fn get_tile_at_index(&self, x: usize, y: usize) -> &BoardTile;
+
+    fn set_tile_at_index(&mut self, x: usize, y: usize, new_tile_value: BoardTile);
+
+    fn get_adjacent_snake_tile_with_value(&self, position: &P, target_value: u16) -> Option<P>;
+}
