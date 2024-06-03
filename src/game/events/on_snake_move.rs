cdf72aaaf10e23ef42b0a5719d157a2f6081b160
PulseSapphire <souravdube17122003@gmail.com>
2024-06-04 01:48:35 +0530

    fix typo, delete double declaration of OnSnakeMove trait.

--- src/game/events/on_snake_move.rs
+++ src/game/events/on_snake_move.rs
@@ -2,7 +2,7 @@ use crate::game::engine::game_state::board::Board;
 use crate::game::events::EventHandler;
 use crate::game::types::position::Position;
 
-pub trait OnSnakeMove<const W: usize, const H: usize, P: Position>: EventHandler {
+
 pub trait OnSnakeMove<const W: usize, const H: usize, P: Position, B: Board<P>>: EventHandler {
     fn on_event(
         &self,
