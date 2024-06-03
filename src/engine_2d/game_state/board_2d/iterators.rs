4a5e113ec3dc252f097bcedba7de83c090fd3743
PulseSapphire <souravdube17122003@gmail.com>
2024-06-04 00:49:17 +0530

    implement BoardIterator for Board2DIterator.

--- src/engine_2d/game_state/board_2d/iterators.rs
+++ src/engine_2d/game_state/board_2d/iterators.rs
@@ -1,4 +1,7 @@
 use crate::engine_2d::game_state::board_2d::Board2D;
+use crate::game::engine::game_state::board::board_tile::BoardTile;
+use crate::game::engine::game_state::board::iterators::BoardIterator;
+use crate::game::types::position::Position2D;
 
 pub struct Board2DIterator<'a, const W: usize, const H: usize> {
     board: &'a Board2D<W, H>,
@@ -14,4 +17,31 @@ impl <'a, const W: usize, const H: usize> Board2DIterator<'a, W, H> {
             col: 0,
         }
     }
+}
+
+impl <'a, const W: usize, const H: usize> BoardIterator<'a, Position2D> for Board2DIterator<'a, W, H> {}
+
+impl <'a, const W: usize, const H: usize> Iterator for Board2DIterator<'a, W, H> {
+
+    type Item = (Position2D, &'a BoardTile);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.col > W {
+            return None;
+        }
+
+        let tile = self.board.get_tile_at_index(self.row, self.col);
+        let item = (Position2D { x: self.row as u8, y: self.col as u8 }, tile);
+
+        if self.row + 1 == H {
+            self.col += 1;
+            self.row = 0;
+        }
+        else {
+            self.row += 1;
+        }
+
+        Some(item)
+    }
+
 }
