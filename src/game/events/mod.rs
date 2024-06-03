d86f9dac1ad17c00395792f431b7ff2519aa6b0b
PulseSapphire <souravdube17122003@gmail.com>
2024-06-04 01:57:04 +0530

    Extend event handling API
    - create Event trait to define basic event structure.
    - create CancellableEvent trait extending Event to support event cancellation with CancellableEvent::is_cancelled and CancellableEvent::cancel_event methods.
    - update EventHandler trait to be generic over event types and include the on_event method.
    - create EventError enum to represent possible errors in event handling.
    - create EventSubject trait with generic event and handler types, including methods for adding and removing event handlers.

--- src/game/events/mod.rs
+++ src/game/events/mod.rs
@@ -1,4 +1,28 @@
 pub mod on_snake_move;
 pub mod on_board_tile_change;
 
-pub trait EventHandler {}
+
+pub trait Event {}
+pub trait CancellableEvent: Event {
+    fn is_cancelled () -> bool;
+    fn cancel_event();
+}
+
+pub trait EventHandler <E: Event> {
+
+    fn on_event(&self, event: E);
+
+}
+
+pub enum EventError {
+    AlreadyRegistered,
+    DoesNotExist,
+}
+
+pub trait EventSubject<E: Event, H: EventHandler<E>> {
+
+    fn add_event_handler (&mut self, event_hanlder: &H) -> Result<(), EventError>;
+
+    fn remove_event_handler (&mut self, event_handler: &H) -> Result<(), EventError>;
+
+}
