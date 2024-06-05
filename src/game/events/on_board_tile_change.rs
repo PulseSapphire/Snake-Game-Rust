b8b1c723d7db9bcadb90427fbedb40d03394a998
PulseSapphire <souravdube17122003@gmail.com>
2024-06-05 23:02:26 +0530

    update OnBoardTileChange trait with new Event API.

--- src/game/events/on_board_tile_change.rs
+++ src/game/events/on_board_tile_change.rs
@@ -1,7 +1,12 @@
 use crate::game::engine::game_state::board::board_tile::BoardTile;
-use crate::game::events::EventHandler;
+use crate::game::events::{Event, EventHandler};
 use crate::game::types::position::Position;
 
-pub trait OnBoardTileChange <P: Position>: EventHandler {
-    fn on_event (new_board_tile: &BoardTile, last_board_tile: &BoardTile, position: &P);
+pub struct BoardTileChangeEvent<'a, P: Position + 'a> {
+    pub new_board_tile: &'a BoardTile,
+    pub last_board_tile: &'a BoardTile,
+    pub position: &'a P,
 }
+
+impl<'a, P: Position> Event for BoardTileChangeEvent<'a, P> {}
+pub trait OnBoardTileChange <'a, P: Position + 'a>: EventHandler<BoardTileChangeEvent<'a, P>> {}
