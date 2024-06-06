034379ffadfea4b631db2964f7d0fa67daebad2e
PulseSapphire <souravdube17122003@gmail.com>
2024-06-06 22:03:13 +0530

    move trait OnBoardTileChangeSubject from on_board_tile_change module to on_board_tile_change::subject.

--- src/game/events/event_types/on_board_tile_change/mod.rs
+++ src/game/events/event_types/on_board_tile_change/mod.rs
@@ -1,31 +1,3 @@
 pub mod handler;
 pub mod subject;
 pub mod event;
-
-use event::BoardTileChangeEvent;
-use handler::OnBoardTileChangeHandler;
-use crate::game::events::{Event, EventError, EventHandler, EventSubject};
-use crate::game::types::position::Position;
-
-pub trait OnBoardTileChangeSubject <'a, P: Position + 'a>: EventSubject<BoardTileChangeEvent<'a, P>, dyn OnBoardTileChangeHandler<'a, P>> {
-    fn add_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError>;
-
-    fn remove_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError>;
-}
-impl <'a, T, P>
-EventSubject<BoardTileChangeEvent<'a, P>, dyn OnBoardTileChangeHandler<'a, P>>
-for T
-where
-    T: OnBoardTileChangeSubject<'a, P>,
-    P: Position
-{
-
-    fn add_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError> {
-        OnBoardTileChangeSubject::add_event_handler(self, event_handler)
-    }
-
-    fn remove_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError> {
-        OnBoardTileChangeSubject::remove_event_handler(self, event_handler)
-    }
-
-}
---
+++ src/game/events/event_types/on_board_tile_change/subject.rs
@@ -0,0 +1,28 @@
+use crate::game::events::event_types::on_board_tile_change::event::BoardTileChangeEvent;
+use crate::game::events::event_types::on_board_tile_change::handler::OnBoardTileChangeHandler;
+use crate::game::events::{EventError, EventSubject};
+use crate::game::types::position::Position;
+
+pub trait OnBoardTileChangeSubject <'a, P: Position + 'a>: EventSubject<BoardTileChangeEvent<'a, P>, dyn OnBoardTileChangeHandler<'a, P>> {
+    fn add_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError>;
+
+    fn remove_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError>;
+}
+
+impl <'a, T, P>
+EventSubject<BoardTileChangeEvent<'a, P>, dyn OnBoardTileChangeHandler<'a, P>>
+for T
+where
+    T: OnBoardTileChangeSubject<'a, P>,
+    P: Position
+{
+
+    fn add_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError> {
+        OnBoardTileChangeSubject::add_event_handler(self, event_handler)
+    }
+
+    fn remove_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError> {
+        OnBoardTileChangeSubject::remove_event_handler(self, event_handler)
+    }
+
+}
