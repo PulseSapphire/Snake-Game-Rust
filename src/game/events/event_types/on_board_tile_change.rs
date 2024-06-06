261b1f4303928c08755ad6191b58a9012e914f5c
PulseSapphire <souravdube17122003@gmail.com>
2024-06-06 21:29:10 +0530

    fix typo: Remove accidentally duplicated fields.

--- src/game/events/event_types/on_board_tile_change.rs
+++ src/game/events/event_types/on_board_tile_change.rs
@@ -3,9 +3,6 @@ use crate::game::events::{Event, EventHandler};
 use crate::game::types::position::Position;
 
 pub struct BoardTileChangeEvent<'a, P: Position + 'a> {
-    pub new_board_tile: &'a BoardTile,
-    pub last_board_tile: &'a BoardTile,
-    pub position: &'a P,
     new_board_tile: &'a BoardTile,
     last_board_tile: &'a BoardTile,
     position: &'a P,
