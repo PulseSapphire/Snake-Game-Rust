d8cb33fb67f36f839aa01e3e1d0c56d3d3ec7a02
PulseSapphire <souravdube17122003@gmail.com>
2024-06-06 21:59:08 +0530

    move struct BoardTileChangeEvent from on_board_tile_change module to on_board_tile_change::event.

---
+++ src/game/events/event_types/on_board_tile_change/event.rs
@@ -0,0 +1,23 @@
+use crate::game::engine::game_state::board::board_tile::BoardTile;
+use crate::game::events::Event;
+use crate::game::types::position::Position;
+
+pub struct BoardTileChangeEvent<'a, P: Position + 'a> {
+    new_board_tile: &'a BoardTile,
+    last_board_tile: &'a BoardTile,
+    position: &'a P,
+}
+
+impl<'a, P: Position + 'a> BoardTileChangeEvent<'a, P> {
+    pub fn get_new_board_tile(&self) -> &'a BoardTile {
+        self.new_board_tile
+    }
+    pub fn get_last_board_tile(&self) -> &'a BoardTile {
+        self.last_board_tile
+    }
+    pub fn get_position(&self) -> &'a P {
+        self.position
+    }
+}
+
+impl<'a, P: Position> Event for BoardTileChangeEvent<'a, P> {}
--- src/game/events/event_types/on_board_tile_change/mod.rs
+++ src/game/events/event_types/on_board_tile_change/mod.rs
@@ -2,29 +2,9 @@ pub mod handler;
 pub mod subject;
 pub mod event;
 
-use crate::game::engine::game_state::board::board_tile::BoardTile;
+use event::BoardTileChangeEvent;
 use crate::game::events::{Event, EventError, EventHandler, EventSubject};
 use crate::game::types::position::Position;
-
-pub struct BoardTileChangeEvent<'a, P: Position + 'a> {
-    new_board_tile: &'a BoardTile,
-    last_board_tile: &'a BoardTile,
-    position: &'a P,
-}
-
-impl<'a, P: Position + 'a> BoardTileChangeEvent<'a, P> {
-    pub fn get_new_board_tile(&self) -> &'a BoardTile {
-        self.new_board_tile
-    }
-    pub fn get_last_board_tile(&self) -> &'a BoardTile {
-        self.last_board_tile
-    }
-    pub fn get_position(&self) -> &'a P {
-        self.position
-    }
-}
-
-impl<'a, P: Position> Event for BoardTileChangeEvent<'a, P> {}
 
 pub trait OnBoardTileChangeHandler<'a, P: Position + 'a>: EventHandler<BoardTileChangeEvent<'a, P>> {
     fn on_event(&self, event: &mut BoardTileChangeEvent<'a, P>);
