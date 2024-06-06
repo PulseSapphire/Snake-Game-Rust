1a6c53fbcdc18dbf7051717343d7337278d8bc58
PulseSapphire <souravdube17122003@gmail.com>
2024-06-06 21:49:41 +0530

    create OnBoardTileChangeSubject.
    - create the trait OnBoardTileChangeSubject.
    - extend EventSubject for OnBoardTileChangeSubject.
    - create impl block for EventSubject to call OnBoardTileChangeHandler::add_event_handler and OnBoardTileChangeHandler::remove_event_handler when EventSubject::add_event_handler and EventSubject::remove_event_handler get called.

--- src/game/events/event_types/on_board_tile_change.rs
+++ src/game/events/event_types/on_board_tile_change.rs
@@ -1,5 +1,5 @@
 use crate::game::engine::game_state::board::board_tile::BoardTile;
-use crate::game::events::{Event, EventHandler};
+use crate::game::events::{Event, EventError, EventHandler, EventSubject};
 use crate::game::types::position::Position;
 
 pub struct BoardTileChangeEvent<'a, P: Position + 'a> {
@@ -33,4 +33,27 @@ where
     fn on_event(&self, event: &mut BoardTileChangeEvent<'a, P>) {
         OnBoardTileChangeHandler::on_event(self, event);
     }
+}
+
+pub trait OnBoardTileChangeSubject <'a, P: Position + 'a>: EventSubject<BoardTileChangeEvent<'a, P>, dyn OnBoardTileChangeHandler<'a, P>> {
+    fn add_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError>;
+
+    fn remove_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError>;
+}
+impl <'a, T, P>
+EventSubject<BoardTileChangeEvent<'a, P>, dyn OnBoardTileChangeHandler<'a, P>>
+for T
+where
+    T: OnBoardTileChangeSubject<'a, P>,
+    P: Position
+{
+
+    fn add_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError> {
+        OnBoardTileChangeSubject::add_event_handler(self, event_handler)
+    }
+
+    fn remove_event_handler (&mut self, event_handler: &dyn OnBoardTileChangeHandler<P>) -> Result<(), EventError> {
+        OnBoardTileChangeSubject::remove_event_handler(self, event_handler)
+    }
+
 }
