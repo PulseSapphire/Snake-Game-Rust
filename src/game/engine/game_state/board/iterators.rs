48c5c793243edd7413e2902f0ba32891c3116224
PulseSapphire <souravdube17122003@gmail.com>
2024-06-09 00:17:04 +0530

    create new traits IterableBoard and IterableBoardMut.

--- src/game/engine/game_state/board/iterators.rs
+++ src/game/engine/game_state/board/iterators.rs
@@ -3,3 +3,6 @@ use crate::game::types::position::Position;
 
 pub trait BoardIterator <'a, P: Position> : Iterator<Item = (P, &'a BoardTile)> {}
 pub trait BoardIteratorMut <'a, P: Position> : Iterator<Item = (P, &'a mut BoardTile)> {}
+
+pub trait IterableBoard<'a, P: Position, I: BoardIterator<'a, P>>: IntoIterator<Item = (P, &'a BoardTile), IntoIter = I> {}
+pub trait IterableBoardMut<'a, P: Position, I: BoardIteratorMut<'a, P>>: IntoIterator<Item = (P, &'a mut BoardTile), IntoIter = I> {}
