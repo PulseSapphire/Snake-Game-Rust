0056517a87f6e5c9f321633142c117c34b09afbf
PulseSapphire <souravdube17122003@gmail.com>
2024-06-08 16:21:39 +0530

    create trait Board2D.

--- src/game/engine/game_state/board/mod.rs
+++ src/game/engine/game_state/board/mod.rs
@@ -1,5 +1,5 @@
 use crate::game::engine::game_state::board::board_tile::BoardTile;
-use crate::game::types::position::Position;
+use crate::game::types::position::{Position, Position2D};
 
 pub mod board_tile;
 pub mod iterators;
@@ -12,4 +12,12 @@ pub trait Board <P: Position> {
     fn get_adjacent_snake_tile_with_value(&self, position: &P, target_value: u16) -> Option<P>;
 
     fn get_dimensions <'a> () -> &'a [u8];
+}
+
+pub trait Board2D: Board<Position2D> {
+    fn get_width() -> u8;
+    fn get_height() -> u8;
+
+    fn get_tile_at_index(&self, x: usize, y: usize) -> &BoardTile;
+    fn set_tile_at_index(&mut self, x: usize, y: usize, new_tile_value: BoardTile);
 }
