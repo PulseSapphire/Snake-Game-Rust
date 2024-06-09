e134dfd6865cb86ab6f58045c19bc6d512dfe500
PulseSapphire <souravdube17122003@gmail.com>
2024-06-09 18:20:50 +0530

    rename OnBoardTileChangeHandler::on_event to on_board_tile_change_event to avoid naming conflicts.
    - The naming conflict forced the usage of fully qualified paths to run the event handlers.
    - change it to something else to allow user to call on_event without having to use fully qualified paths.
    - These solutions are currently being put in place in the absense of trait aliases. Or more so due to them not being available in stable versions yet.

--- src/game/events/event_types/on_board_tile_change/handler.rs
+++ src/game/events/event_types/on_board_tile_change/handler.rs
@@ -5,7 +5,7 @@ use crate::game::types::position::Position;
 pub trait OnBoardTileChangeHandler<'a, P: Position + 'a>:
     EventHandler<BoardTileChangeEvent<'a, P>>
 {
-    fn on_event(&self, event: &mut BoardTileChangeEvent<'a, P>);
+    fn on_board_tile_change_event(&self, event: &mut BoardTileChangeEvent<'a, P>);
 }
 
 impl<'a, T, P> EventHandler<BoardTileChangeEvent<'a, P>> for T
@@ -14,6 +14,6 @@ where
     P: Position,
 {
     fn on_event(&self, event: &mut BoardTileChangeEvent<'a, P>) {
-        OnBoardTileChangeHandler::on_event(self, event);
+        self.on_board_tile_change_event(event);
     }
 }
