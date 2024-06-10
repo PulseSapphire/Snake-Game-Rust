b6358b1cb4009d3229abf403b57cd5558d54f58f
PulseSapphire <souravdube17122003@gmail.com>
2024-06-10 21:48:53 +0530

    fix lifetime parameters and usages of them in OnBoardTileChangeSubject trait.

--- src/game/events/event_types/on_board_tile_change/subject.rs
+++ src/game/events/event_types/on_board_tile_change/subject.rs
@@ -3,35 +3,35 @@ use crate::game::events::event_types::on_board_tile_change::handler::OnBoardTile
 use crate::game::events::{EventError, EventSubject};
 use crate::game::types::position::Position;
 
-pub trait OnBoardTileChangeSubject<'a, P: Position + 'a>:
-    EventSubject<BoardTileChangeEvent<'a, P>, dyn OnBoardTileChangeHandler<'a, P>>
+pub trait OnBoardTileChangeSubject<'a, P: Position>:
+    for<'b> EventSubject<'a, BoardTileChangeEvent<'b, P>, dyn OnBoardTileChangeHandler<P>>
 {
     fn add_on_board_tile_change_event_handler(
         &mut self,
-        event_handler: &dyn OnBoardTileChangeHandler<P>,
+        event_handler: &'a dyn OnBoardTileChangeHandler<P>,
     ) -> Result<(), EventError>;
 
     fn remove_on_board_tile_change_event_handler(
         &mut self,
-        event_handler: &dyn OnBoardTileChangeHandler<P>,
+        event_handler: &'a dyn OnBoardTileChangeHandler<P>,
     ) -> Result<(), EventError>;
 }
 
-impl<'a, T, P> EventSubject<BoardTileChangeEvent<'a, P>, dyn OnBoardTileChangeHandler<'a, P>> for T
+impl<'a, T, P> EventSubject<'a, BoardTileChangeEvent<'_, P>, dyn OnBoardTileChangeHandler<P>> for T
 where
     T: OnBoardTileChangeSubject<'a, P>,
     P: Position,
 {
     fn add_event_handler(
         &mut self,
-        event_handler: &dyn OnBoardTileChangeHandler<P>,
+        event_handler: &'a dyn OnBoardTileChangeHandler<P>,
     ) -> Result<(), EventError> {
         self.add_on_board_tile_change_event_handler(event_handler)
     }
 
     fn remove_event_handler(
         &mut self,
-        event_handler: &dyn OnBoardTileChangeHandler<P>,
+        event_handler: &'a dyn OnBoardTileChangeHandler<P>,
     ) -> Result<(), EventError> {
         self.remove_on_board_tile_change_event_handler(event_handler)
     }
