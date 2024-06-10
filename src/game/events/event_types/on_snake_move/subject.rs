3f4aef8dbd5c644da843dd1d194b00bf86916966
PulseSapphire <souravdube17122003@gmail.com>
2024-06-10 21:41:02 +0530

    update OnSnakeMoveSubject with proper lifetime parameters for EventSubject trait.

--- src/game/events/event_types/on_snake_move/subject.rs
+++ src/game/events/event_types/on_snake_move/subject.rs
@@ -3,35 +3,35 @@ use crate::game::events::event_types::on_snake_move::handler::OnSnakeMoveHandler
 use crate::game::events::{EventError, EventSubject};
 use crate::game::types::position::Position;
 
-pub trait OnSnakeMoveSubject<P: Position>:
-    for<'b> EventSubject<SnakeMoveEvent<'b, P>, dyn OnSnakeMoveHandler<P>>
+pub trait OnSnakeMoveSubject<'a, P: Position>:
+    for<'b> EventSubject<'a, SnakeMoveEvent<'b, P>, dyn OnSnakeMoveHandler<P>>
 {
     fn add_on_snake_move_event_handler(
         &mut self,
-        event_handler: &dyn OnSnakeMoveHandler<P>,
+        event_handler: &'a dyn OnSnakeMoveHandler<P>,
     ) -> Result<(), EventError>;
 
     fn remove_on_snake_move_event_handler(
         &mut self,
-        event_handler: &dyn OnSnakeMoveHandler<P>,
+        event_handler: &'a dyn OnSnakeMoveHandler<P>,
     ) -> Result<(), EventError>;
 }
 
-impl<T, P> EventSubject<SnakeMoveEvent<'_, P>, dyn OnSnakeMoveHandler<P>> for T
+impl<'a, T, P> EventSubject<'a, SnakeMoveEvent<'_, P>, dyn OnSnakeMoveHandler<P>> for T
 where
-    T: OnSnakeMoveSubject<P>,
+    T: OnSnakeMoveSubject<'a, P>,
     P: Position,
 {
     fn add_event_handler(
         &mut self,
-        event_handler: &dyn OnSnakeMoveHandler<P>,
+        event_handler: &'a dyn OnSnakeMoveHandler<P>,
     ) -> Result<(), EventError> {
         self.add_on_snake_move_event_handler(event_handler)
     }
 
     fn remove_event_handler(
         &mut self,
-        event_handler: &dyn OnSnakeMoveHandler<P>,
+        event_handler: &'a dyn OnSnakeMoveHandler<P>,
     ) -> Result<(), EventError> {
         self.remove_on_snake_move_event_handler(event_handler)
     }
