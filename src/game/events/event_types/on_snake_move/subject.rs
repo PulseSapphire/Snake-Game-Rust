c2c08161d27474e8061a15c4956d0e9f683900b1
PulseSapphire <souravdube17122003@gmail.com>
2024-06-10 20:40:50 +0530

    Remove unnecessary lifetime parameters from the OnSnakeMoveSubject trait.
    - Remove unnecessary lifetime parameters that were previously needed due to lifetime parameters in OnSnakeMoveHandler's signature.
    - Made OnSnakeMoveSubject much easier to implement and use.

--- src/game/events/event_types/on_snake_move/subject.rs
+++ src/game/events/event_types/on_snake_move/subject.rs
@@ -3,8 +3,8 @@ use crate::game::events::event_types::on_snake_move::handler::OnSnakeMoveHandler
 use crate::game::events::{EventError, EventSubject};
 use crate::game::types::position::Position;
 
-pub trait OnSnakeMoveSubject<'a, P: Position + 'a>:
-    EventSubject<SnakeMoveEvent<'a, P>, dyn OnSnakeMoveHandler<'a, P>>
+pub trait OnSnakeMoveSubject<P: Position>:
+    for<'b> EventSubject<SnakeMoveEvent<'b, P>, dyn OnSnakeMoveHandler<P>>
 {
     fn add_on_snake_move_event_handler(
         &mut self,
@@ -17,9 +17,9 @@ pub trait OnSnakeMoveSubject<'a, P: Position + 'a>:
     ) -> Result<(), EventError>;
 }
 
-impl<'a, T, P> EventSubject<SnakeMoveEvent<'a, P>, dyn OnSnakeMoveHandler<'a, P>> for T
+impl<T, P> EventSubject<SnakeMoveEvent<'_, P>, dyn OnSnakeMoveHandler<P>> for T
 where
-    T: OnSnakeMoveSubject<'a, P>,
+    T: OnSnakeMoveSubject<P>,
     P: Position,
 {
     fn add_event_handler(
